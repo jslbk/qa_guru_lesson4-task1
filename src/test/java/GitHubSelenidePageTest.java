3a1d40201f351aa80b3808370f99ea99ffeb687c
Julyan SLABKO <115033204+jslbk@users.noreply.github.com>
2023-10-31 22:07:39 +0200

    method name improvement

--- src/test/java/GitHubSelenidePageTest.java
+++ src/test/java/GitHubSelenidePageTest.java
@@ -13,13 +13,13 @@ class GitHubSelenidePageTest {
         $(".header-search-button").click();
         $("#query-builder-test").setValue("Selenide").pressEnter();
         $("[data-testid=results-list]").shouldBe(visible);
-        openFirstResultAtList();
+        openFirstResultOnList();
         $("#wiki-tab").parent().click();
         $("#wiki-pages-filter").setValue("SoftAssertions");
         $$(".wiki-rightbar:not([style='display: none'])").shouldHave(size(1));
     }
 
-    private void openFirstResultAtList() {
+    private void openFirstResultOnList() {
         $$("[data-testid=results-list] a").first().click();
         $("[itemprop=name]").shouldBe(visible).shouldHave(text("selenide"));
     }
