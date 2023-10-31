6ba821c5f9b296faffb9da3f735f35758578e6e9
Julyan SLABKO <115033204+jslbk@users.noreply.github.com>
2023-10-31 19:44:53 +0200

    initial commit: softAsgit rm --cached -r .

---
+++ src/test/java/GitHubSelenidePageTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+class GitHubSelenidePageTest {
+
+    @Test
+    void softAssertionsSectionTest() {
+        open("https://github.com/");
+        $(".header-search-button").click();
+        $("#query-builder-test").setValue("Selenide").pressEnter();
+        $("[data-testid=results-list]").shouldBe(visible);
+        openFirstResultAtList();
+        $("#wiki-tab").parent().click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $$(".wiki-rightbar:not([style='display: none'])").shouldHave(size(1));
+    }
+
+    private void openFirstResultAtList() {
+        $$("[data-testid=results-list] a").first().click();
+        $("[itemprop=name]").shouldBe(visible).shouldHave(text("selenide"));
+    }
+
+}
