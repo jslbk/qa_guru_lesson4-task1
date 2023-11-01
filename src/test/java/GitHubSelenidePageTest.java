b00d2f0bb2afc41018cceddfef5138b456d6eb45
Julyan SLABKO <115033204+jslbk@users.noreply.github.com>
2023-11-01 23:50:49 +0200

    extended with missed check

--- src/test/java/GitHubSelenidePageTest.java
+++ src/test/java/GitHubSelenidePageTest.java
@@ -1,8 +1,10 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class GitHubSelenidePageTest {
@@ -14,10 +16,21 @@ class GitHubSelenidePageTest {
         $("#query-builder-test").setValue("Selenide").pressEnter();
         $("[data-testid=results-list]").shouldBe(visible);
         openFirstResultOnList();
-        $("#wiki-tab").parent().click();
+
+        // Click 'Wiki' tab and check rightbar contains 'SoftAssertions' option
+        $("#wiki-tab").click();
         $("#wiki-pages-filter").setValue("SoftAssertions");
-        $$(".wiki-rightbar:not([style='display: none'])").shouldHave(size(1));
+        $("#wiki-pages-filter").shouldHave(value("SoftAssertions"));
+        $$(".wiki-rightbar").filterBy(Condition.not(hidden)).shouldHave(size(1));
+
+        // Click on the "SoftAssertions"
+        $("a[href*='SoftAssertions']").click();
+
+        // Find code example title and check there are code area that contains text 'Test'
+        $("h4 [href*='junit5']").shouldBe(visible, Duration.ofSeconds(5));
+        $("h4 [href*='junit5']").parent().sibling(0).shouldHave(text("Test"));
     }
+
 
     private void openFirstResultOnList() {
         $$("[data-testid=results-list] a").first().click();
